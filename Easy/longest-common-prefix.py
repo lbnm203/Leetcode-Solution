2cbf418926dfc96eb555c58ead395b4d20c14cfa
lbnm203 <lebanhatminh1303@gmail.com>
2024-06-21 12:08:29 +0700

    Change 14. Longest Common Prefix

--- Easy/longest-common-prefix.py
+++ Easy/longest-common-prefix.py
@@ -1,13 +1,21 @@
 def longest_common_prefix(strs):
-    f_str = strs[0]
-    l_str = strs[-1]
-    lst = ""
-    for i in range(len(strs)):
-        if f_str[i] != l_str[i]:
-            break
+    if not strs:
+        return ""
+
+    if len(strs) == 1:
+        return strs[0]
+
+    strs.sort()
+    first_str = strs[0]
+    last_str = strs[-1]
+    prefix = ""
+    for i in range(len(first_str)):
+        if i < len(last_str) and first_str[i] == last_str[i]:
+            prefix += first_str[i]
         else:
-            lst += f_str[i]
-    return lst
+            break
+
+    return prefix
 
 
 strs = ["flower", "flow", "flight"]
